888aa961a6abe4cb3b8f9724eed015912bfeb1db
Jomel Edodollon <jomeledodollon@yahoo.ca>
2022-01-19 17:18:21 -0800

    created files

---
+++ fetcher.js
@@ -0,0 +1,4 @@
+const request = require('request');
+// Async operations:
+//  make https request and wait for response
+//  AFTER request is complete take data recieved and write it to file in locel system
